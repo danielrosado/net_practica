4b30b14cfca8e694840da5b08907ea850cd9b5f0
Daniel Rosado <danieljesus.rosado@alumnos.upm.es>
2021-02-20 12:57:44 +0100

    Volcar Carrito de la Compra a la tabla Pedidos

--- TiendaVirtual/Controllers/CarritoCompraController.cs
+++ TiendaVirtual/Controllers/CarritoCompraController.cs
@@ -9,10 +9,30 @@ namespace TiendaVirtual.Controllers
 {
     public class CarritoCompraController : Controller
     {
+        private TiendaVirtualContainer db = new TiendaVirtualContainer();
+
         // GET: CarritoCompra
         public ActionResult Index(CarritoCompra cc)
         {
             return View(cc);
+        }
+
+        public ActionResult VolcarCarrito(CarritoCompra cc)
+        {
+            foreach (Producto producto in cc)
+            {
+                Pedido pedido = new Pedido();
+                pedido.IdProducto = producto.Id;
+                //TODO cantidad
+                pedido.Usuario = User.Identity.Name;
+                pedido.Fecha = DateTime.Now;
+
+                db.Pedidos.Add(pedido);
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Productos");
         }
     }
 }
