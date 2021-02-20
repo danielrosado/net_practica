ed091dbc1721eb499985c15227ff8bec3e06a718
Daniel Rosado <danieljesus.rosado@alumnos.upm.es>
2021-02-20 12:01:45 +0100

    Visualizar el carrito de la compra

---
+++ TiendaVirtual/Controllers/CarritoCompraController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtual.Models;
+
+namespace TiendaVirtual.Controllers
+{
+    public class CarritoCompraController : Controller
+    {
+        // GET: CarritoCompra
+        public ActionResult Index(CarritoCompra cc)
+        {
+            return View(cc);
+        }
+    }
+}
